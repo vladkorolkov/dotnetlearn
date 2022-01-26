d624b57fa4b5eb382b65c381ca8248e87a3e7e55
Vlad <korolkovjazz@yandex.ru>
2022-01-26 20:53:02 +0300

    json read

--- dotnetlearn/lesson_1/Program.cs
+++ dotnetlearn/lesson_1/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+namespace lesson_1;
+public class Program
+{
+    public static void Main()
+    {
+        var file = File.ReadAllText("all_deals.json");
+
+        List<Deal> deals = JsonSerializer.Deserialize<List<Deal>>(file);
+
+    }
+}
