47215f67c4ff7e2dc5f9b109f9861346e683c6a2
Vlad <korolkovjazz@yandex.ru>
2022-02-03 16:30:35 +0300

    первая часть задания

--- dotnetlearn/lesson_1_2/Program.cs
+++ dotnetlearn/lesson_1_2/Program.cs
@@ -1,18 +1,31 @@
 ﻿using Dadata;
 using Dadata.Model;
 
+bool isWorking = true;
 var token = "2dee0cdd0703e88bd28fd6b064629ee65d7fb225";
 var secret = "638e0c0799ce0d1fcd992ac7000b336aa7a93f65";
 Console.WriteLine("Введите ИНН организации: ");
-var query = Console.ReadLine();
-GetInfo(query, token);
+while (isWorking)
+{   
+    var query = Console.ReadLine();  
+    GetInfo(query, token);
+}
 
-Console.ReadLine();
-
-static async void GetInfo(string inn, string token)
+static async void GetInfo(string query, string token)
 {
     var api = new SuggestClientAsync(token);
-    var response = await api.FindParty("7707083893");
-    var partyName = response.suggestions[0].value;
-    Console.WriteLine(partyName);
+    try
+    {
+        var response = await api.FindParty(query);
+        var partyName = response.suggestions[0].value;
+        Console.WriteLine("Найдено: "+partyName);
+        Console.WriteLine("\nДля нового поиска нажмите любую клавишу");
+    }
+    catch 
+    {
+        Console.WriteLine("По вашему запросу ничего не найдено.\n");
+        Console.WriteLine("Введите ИНН организации: ");
+    }
+
+
 }
